ceb2e03b7c378d080218d9a687d39b303fe4fac0
sam <sam@ricorocks.agency>
2021-01-31 00:13:21 +0000

    Tidy up

--- src/Engines/SqliteEngine.php
+++ src/Engines/SqliteEngine.php
@@ -11,34 +11,34 @@ use Nedwors\Hopper\Database;
 class SqliteEngine implements Engine
 {
     protected Filer $filer;
-    protected $databasePath;
 
     public function __construct(Filer $filer)
     {
         $this->filer = $filer;
-        $this->databasePath = Str::finish(config('hopper.drivers.sqlite.database-path'), '/');
     }
 
     public function use(string $database)
     {
-        $database = $this->lookupAlias($database);
+        $database = $this->isAlias($database)
+                        ? config('hopper.default-database')
+                        : $database;
 
         $this->createIfNeeded($database);
 
         $this->filer->setCurrentHop($database);
     }
 
-    protected function lookupAlias($database)
+    protected function isAlias($database)
     {
         if (!$gitBranch = config('hopper.default-branch')) {
-            return $database;
+            return false;
         }
 
         if ($database != $gitBranch) {
-            return $database;
+            return false;
         }
 
-        return config('hopper.default-database');
+        return true;
     }
 
     protected function createIfNeeded($database)
@@ -47,7 +47,7 @@ class SqliteEngine implements Engine
             return;
         }
 
-        $fileName = $this->normalize($database);
+        $fileName = $this->toFilePath($database);
 
         if ($this->exists($fileName)) {
             return;
@@ -66,13 +66,13 @@ class SqliteEngine implements Engine
         $database = $this->filer->currentHop();
 
         return $database
-            ? new Database($database, $this->normalize($database), 'sqlite')
+            ? new Database($database, $this->toFilePath($database), 'sqlite')
             : null;
     }
 
     public function exists(string $database): bool
     {
-        return File::exists($this->normalize($database));
+        return File::exists($this->toFilePath($database));
     }
 
     public function delete(string $database): bool
@@ -81,15 +81,20 @@ class SqliteEngine implements Engine
             return false;
         }
 
-        return File::delete($this->normalize($database));
+        return File::delete($this->toFilePath($database));
     }
 
-    protected function normalize(string $database): string
+    protected function toFilePath(string $database): string
     {
         if (!$this->isDefault($database)) {
-            $database = $this->databasePath . $database;
+            $database = $this->applyDatabasePath($database);
         }
 
         return database_path(Str::finish($database, '.sqlite'));
+    }
+
+    protected function applyDatabasePath($database)
+    {
+        return Str::finish(config('hopper.drivers.sqlite.database-path'), '/') . $database;
     }
 }
