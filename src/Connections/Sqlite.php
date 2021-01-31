bef3d5ea26ee113e3c15aaf9030c14f5ad87b2b9
sam <sam@ricorocks.agency>
2021-01-31 21:25:22 +0000

    Adds a Sqlite connection

---
+++ src/Connections/Sqlite.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Nedwors\Hopper\Connections;
+
+use Illuminate\Support\Facades\File;
+use Nedwors\Hopper\Contracts\Connection;
+use Illuminate\Support\Str;
+use Nedwors\Hopper\Database;
+
+class Sqlite implements Connection
+{
+    public function create(string $name)
+    {
+        $fileName = $this->toFilePath($name);
+
+        if ($this->exists($fileName)) {
+            return;
+        }
+
+        File::put($fileName, '');
+    }
+
+    public function exists(string $name): bool
+    {
+        return File::exists($this->toFilePath($name));
+    }
+
+    public function delete(string $name): bool
+    {
+        if ($this->isDefault($name)) {
+            return false;
+        }
+
+        return File::delete($this->toFilePath($name));
+    }
+
+    public function database(string $name): Database
+    {
+        return new Database($name, $this->toFilePath($name), 'sqlite');
+    }
+
+    protected function toFilePath(string $name): string
+    {
+        if (!$this->isDefault($name)) {
+            $name = $this->applyDatabasePath($name);
+        }
+
+        return database_path(Str::finish($name, '.sqlite'));
+    }
+
+    protected function applyDatabasePath($database)
+    {
+        return Str::finish(config('hopper.drivers.sqlite.database-path'), '/') . $database;
+    }
+
+    protected function isDefault($name)
+    {
+        return $name === config('hopper.default-database');
+    }
+}
---
+++ src/Contracts/Connection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nedwors\Hopper\Contracts;
+
+use Nedwors\Hopper\Database;
+
+interface Connection
+{
+    public function create(string $name);
+
+    public function exists(string $name): bool;
+
+    public function delete(string $name): bool;
+
+    public function database(string $name): Database;
+}
