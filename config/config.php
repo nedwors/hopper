10f7a8672e0400239eceb17858f1c8ef42f2c0f9
sam <sam@ricorocks.agency>
2021-02-15 17:09:31 +0000

    Removes redundant BootCheck

--- config/config.php
+++ config/config.php
@@ -1,13 +1,11 @@
 <?php
 
-use Nedwors\Hopper\BootChecks\AppKey;
 use Nedwors\Hopper\BootChecks\Environment;
 
 return [
     'default-branch' => 'main',
 
     'boot-checks' => [
-        AppKey::class,
         Environment::class
     ]
 ];
