79dc99d60b82bbe41beb8471512e97aad6af5890
sam <nedwors@gmail.com>
2021-02-22 09:25:52 +0000

    Adds a trait for EventListeners

--- src/Console/DeleteCommand.php
+++ src/Console/DeleteCommand.php
@@ -3,14 +3,13 @@
 namespace Nedwors\Hopper\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Event;
-use Nedwors\Hopper\Events\DatabaseDeleted;
-use Nedwors\Hopper\Events\DatabaseNotDeleted;
-use Nedwors\Hopper\Events\HoppedToDefault;
 use Nedwors\Hopper\Facades\Hop;
+use Nedwors\Hopper\Traits\ListensForEvents;
 
 class DeleteCommand extends Command
 {
+    use ListensForEvents;
+
     protected $signature = 'hop:delete {database?}';
 
     protected $description = 'Delete the given hopper database';
@@ -23,28 +22,5 @@ class DeleteCommand extends Command
 
         $this->listen();
         Hop::delete($database);
-    }
-
-    protected function listen()
-    {
-        Event::listen(DatabaseDeleted::class, function ($event) {
-            $this->info("Successfully deleted <fg=yellow>$event->name</>");
-        });
-
-        Event::listen(DatabaseNotDeleted::class, function ($event) {
-            $this->info("<fg=yellow>$event->name</> {$this->notDeletedMessage($event->reason)}, so it was not deleted");
-        });
-
-        Event::listen(HoppedToDefault::class, function ($event) {
-            $this->info("Hopped to default database: <fg=yellow>$event->name</>");
-        });
-    }
-
-    protected function notDeletedMessage($reason)
-    {
-        return [
-            DatabaseNotDeleted::DOES_NOT_EXIST => 'does not exist',
-            DatabaseNotDeleted::DEFAULT => 'is the default database',
-        ][$reason];
     }
 }
--- src/Console/HopCommand.php
+++ src/Console/HopCommand.php
@@ -3,15 +3,15 @@
 namespace Nedwors\Hopper\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Event;
-use Nedwors\Hopper\Events\DatabaseCreated;
-use Nedwors\Hopper\Events\HoppedToDatabase;
-use Nedwors\Hopper\Events\HoppedToDefault;
 use Nedwors\Hopper\Facades\Git;
 use Nedwors\Hopper\Facades\Hop;
+use Nedwors\Hopper\Events\DatabaseCreated;
+use Nedwors\Hopper\Traits\ListensForEvents;
 
 class HopCommand extends Command
 {
+    use ListensForEvents;
+
     protected $signature = 'hop {database?}';
 
     protected $description = 'Hop to the given database';
@@ -34,20 +34,10 @@ class HopCommand extends Command
         $this->postCreationCallback->__invoke();
     }
 
-    protected function listen()
+    protected function databaseCreated(DatabaseCreated $event, $message)
     {
-        Event::listen(DatabaseCreated::class, function ($event) {
-            $this->info("<fg=yellow>$event->name</> was created");
-            $this->postCreationCallback = $this->postCreationCallback($event->name);
-        });
-
-        Event::listen(HoppedToDatabase::class, function ($event) {
-            $this->info("Hopped to <fg=yellow>$event->name</>");
-        });
-
-        Event::listen(HoppedToDefault::class, function ($event) {
-            $this->info("Hopped to default database: <fg=yellow>$event->name</>");
-        });
+        $message();
+        $this->postCreationCallback = $this->postCreationCallback($event->name);
     }
 
     protected function postCreationCallback($name)
