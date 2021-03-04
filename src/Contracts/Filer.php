4243efbe6e9aa337a46d6cd44f7f7307bfeea877
sam <nedwors@gmail.com>
2021-03-04 11:13:05 +0000

    Adds phpdocs to filer contract

--- src/Contracts/Filer.php
+++ src/Contracts/Filer.php
@@ -4,9 +4,18 @@ namespace Nedwors\Hopper\Contracts;
 
 interface Filer
 {
+    /**
+     * Stores the current database name
+     */
     public function setCurrentHop(string $database);
 
+    /**
+     * Nullfies the current database name
+     * */
     public function flushCurrentHop();
 
+    /**
+     * Returns the current database name, or null if none is set
+     * */
     public function currentHop(): ?string;
 }
