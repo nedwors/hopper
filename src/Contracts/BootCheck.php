8b84db59226bb1e627450ac7c503e48bf8ada1d6
sam <nedwors@gmail.com>
2021-03-04 11:16:01 +0000

    Adds phpdoc to BootCheck

--- src/Contracts/BootCheck.php
+++ src/Contracts/BootCheck.php
@@ -4,5 +4,8 @@ namespace Nedwors\Hopper\Contracts;
 
 interface BootCheck
 {
+    /**
+     * Determines if Hopper should configure the database connection
+     * */
     public function check(): bool;
 }
